56f9b1877ec7eed2660993e453686ac259444520
Richieon88 <>
2023-10-09 18:36:25 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to the Quiz")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play!")
+
+answer = input("Whats the capital of France? ")
+if answer == "paris":
+    print("Correct!")
+else:
+    print("Incorrect!") 
+    
+answer = input("Whats the capital of France? ")
+if answer == "paris":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Whats the capital of France? ")
+if answer == "paris":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Whats the capital of France? ")
+if answer == "paris":
+    print("Correct!")
+else:
+    print("Incorrect!")               
