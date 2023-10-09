8f0ec5eb32b379f502accba79b923a74fa253bbe
Richieon88 <>
2023-10-09 22:12:18 +0000

    Add score to quiz

--- run.py
+++ run.py
@@ -1,32 +1,38 @@
 print("Welcome to the Quiz")
-
+score = 0
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play!")
 
 answer = input("Whats the capital of France? ")
-if answer == "paris":
+if answer.lower() == "paris":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!") 
     
 answer = input("Whats the capital of Spain? ")
-if answer == "madrid":
+if answer.lower() == "madrid":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("Whats the capital of England? ")
-if answer == "london":
+if answer.lower() == "london":
     print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
 answer = input("Whats the capital of Germay? ")
-if answer == "berlin":
+if answer.lower() == "berlin":
     print("Correct!")
+    score += 1
 else:
-    print("Incorrect!")               
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct")                   
